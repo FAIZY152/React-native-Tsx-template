053189bfa8ba70cdafda23cb37ebe8c27aabb111
FAIZY152 <fayazjani7576@gmail.com>
2025-09-18 11:00:26 +0500

    add-initial-commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function index() {
+  return (
+    <View>
+      <Text>Hello world </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
